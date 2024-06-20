de5e27307fb40108e62076a9cdd60871319f78e2
USER 1995 <userfrom1995@gmail.com>
2024-06-20 15:21:40 +0000

    Modified to make browser headless

--- 2.Automate The News/1.news-extract-data.py
+++ 2.Automate The News/1.news-extract-data.py
@@ -1,13 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import pandas as pd
 
-web = 'https://www.thesun.co.uk/sport/football/'
-path = '/Users/frankandrade/Downloads/chromedriver'  # introduce path here
+# Define path to chromedriver executable
+path = './chromedriver-linux64/chromedriver'
 
-# Creating the driver
-driver_service = Service(executable_path=path)
-driver = webdriver.Chrome(service=driver_service)
+# Configure Chrome options
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-renderer-backgrounding")
+options.add_argument("--disable-background-timer-throttling")
+options.add_argument("--disable-backgrounding-occluded-windows")
+options.add_argument("--disable-client-side-phishing-detection")
+options.add_argument("--disable-crash-reporter")
+options.add_argument("--disable-oopr-debug-crash-dump")
+options.add_argument("--no-crash-upload")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-extensions")
+options.add_argument("--disable-low-res-tiling")
+options.add_argument("--log-level=3")
+options.add_argument("--silent")
+options.add_argument("--headless")  # Enable headless mode
+
+# Initialize Chrome webdriver with options
+driver = webdriver.Chrome( options=options)
+
+# URL to scrape
+web = 'https://www.thesun.co.uk/sport/football/'
+
+# Open the webpage
 driver.get(web)
 
 # Finding Elements
@@ -17,16 +40,20 @@ titles = []
 subtitles = []
 links = []
 for container in containers:
-    title = container.find_element(by='xpath', value='./a/h2').text
-    subtitle = container.find_element(by='xpath', value='./a/p').text
-    link = container.find_element(by='xpath', value='./a').get_attribute('href')
-    titles.append(title)
-    subtitles.append(subtitle)
-    links.append(link)
+    try:
+        title = container.find_element(by='xpath', value='.//a/h2').text
+        subtitle = container.find_element(by='xpath', value='.//a/p').text
+        link = container.find_element(by='xpath', value='.//a').get_attribute('href')
+        titles.append(title)
+        subtitles.append(subtitle)
+        links.append(link)
+    except Exception as e:
+        print(f"Error: {e}")
 
 # Exporting data to a CSV file
 my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}
 df_headlines = pd.DataFrame(my_dict)
-df_headlines.to_csv('headline.csv')
+df_headlines.to_csv('headline.csv', index=False)
 
+# Quit the driver
 driver.quit()
